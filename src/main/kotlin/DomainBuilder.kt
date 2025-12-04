5e41ed5d984fb20ce9f70ccd8d7ebcf3b21d50bc
Yasmeen Ali  Abu Dib <yasmeenaalii30520055@gmail.com>
2025-12-04 19:57:10 +0200

    Add buildTeams funcation featuer

--- src/main/kotlin/DomainBuilder.kt
+++ src/main/kotlin/DomainBuilder.kt
@@ -11,19 +11,21 @@ class DomainBuilder(
     private val rawMentees: List<MenteeRaw>,
     private val rawSubmissions: List<PerformanceRaw>
 ) {
-
-    private fun buildTeams(rawTeams: List<TeamRaw>): MutableMap<String, Team> {
+      private fun buildTeam(teamRaw: TeamRaw): Team {
+        return Team(
+            id = teamRaw.id,
+            name = teamRaw.name,
+            mentor = teamRaw.mentorLead,
+            mentees = mutableListOf()
+        )
+    }
+    private fun buildTeamsMap(): MutableMap<String, Team> {
         val teamsById = mutableMapOf<String, Team>()
         rawTeams.forEach { teamRaw ->
-            val team = Team(
-                id = teamRaw.id,
-                name = teamRaw.name,
-                mentor = teamRaw.mentorLead,
-                mentees = mutableListOf()
-            )
-            teamsById[teamRaw.id] = team
+            teamsById[teamRaw.id] = buildTeam(teamRaw)
         }
-        return teamsById 
+        return teamsById
+    }
     }
 
     private fun buildMentees(
