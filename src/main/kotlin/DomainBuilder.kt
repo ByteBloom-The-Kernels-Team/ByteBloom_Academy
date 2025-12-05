f4b48cbb7b2c8f03c7b399e60fc53e003c447572
Mona Abu Nada <monaabonada409755535@gmail.com>
2025-12-05 10:40:27 +0200

    Update buildMentee function feature

--- src/main/kotlin/DomainBuilder.kt
+++ src/main/kotlin/DomainBuilder.kt
@@ -1,4 +1,3 @@
-
 import models.TeamRaw
 import models.MenteeRaw
 import models.PerformanceRaw
@@ -11,7 +10,7 @@ class DomainBuilder(
     private val rawMentees: List<MenteeRaw>,
     private val rawSubmissions: List<PerformanceRaw>
 ) {
-      private fun buildTeam(teamRaw: TeamRaw): Team {
+    private fun buildTeam(teamRaw: TeamRaw): Team {
         return Team(
             id = teamRaw.id,
             name = teamRaw.name,
@@ -19,6 +18,7 @@ class DomainBuilder(
             mentees = mutableListOf()
         )
     }
+
     private fun buildTeamsMap(): MutableMap<String, Team> {
         val teamsById = mutableMapOf<String, Team>()
         rawTeams.forEach { teamRaw ->
@@ -26,31 +26,36 @@ class DomainBuilder(
         }
         return teamsById
     }
+
+    private fun buildMentee(
+        menteeRaw: MenteeRaw,
+        team: Team,
+        submissions: List<PerformanceSubmission>
+    ): Mentee {
+        return Mentee(
+            menteeRaw.id,
+            menteeRaw.name,
+            team,
+            submissions
+        )
     }
 
-    private fun buildMentees(
-        rawMentees: List<MenteeRaw>,
+    private fun attachMenteesToTeams(
         teamsById: MutableMap<String, Team>,
-        submissionsByMentee: MutableMap<String, List<PerformanceSubmission>>
+        submissionsByMentee: Map<String, List<PerformanceSubmission>>
     ) {
         rawMentees.forEach { menteeRaw ->
             val team = teamsById[menteeRaw.teamId]
             if (team != null) {
                 val submissions = submissionsByMentee[menteeRaw.id] ?: emptyList()
-
-                val mentee = Mentee(
-                    id = menteeRaw.id,
-                    name = menteeRaw.name,
-                    team = team,
-                    submissions = submissions
-                )
-
+                val mentee = buildMentee(menteeRaw, team, submissions)
                 team.mentees.add(mentee)
             }
         }
     }
 
-    private fun buildPerformanceSubmissions(rawSubmissions: List<PerformanceRaw>): MutableMap<String, List<PerformanceSubmission>>{
+
+    private fun buildPerformanceSubmissions(rawSubmissions: List<PerformanceRaw>): MutableMap<String, List<PerformanceSubmission>> {
         val submissionByMentee = mutableMapOf<String, List<PerformanceSubmission>>()
 
         rawSubmissions.forEach { submissionRaw ->
