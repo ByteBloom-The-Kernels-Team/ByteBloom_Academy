cf1cc7d044bd0e291d36a3701c2a187ff791183a
Doaa Allaham <yahyadoaa25@gmail.com>
2025-12-23 23:35:23 +0200

    Define-repository-interfaces

--- src/main/kotlin/repository/CsvAttendanceRepository.kt
+++ src/main/kotlin/repository/CsvAttendanceRepository.kt
@@ -1,2 +1,19 @@
 package repository
 
+import domain.Attendance
+import parseAttendanceData
+
+class CsvAttendanceRepository : AttendanceRepository {
+
+    override fun getAll(): List<Attendance> {
+        return parseAttendanceData().map { raw ->
+            Attendance(
+                menteeId = raw.menteeId,
+                week1Status = raw.week1Status,
+                week2Status = raw.week2Status,
+                week3Status = raw.week3Status
+            )
+        }
+    }
+}
+
--- src/main/kotlin/repository/CsvProjectRepository.kt
+++ src/main/kotlin/repository/CsvProjectRepository.kt
@@ -1,2 +1,17 @@
 package repository
 
+import domain.Project
+import parseProjectData
+
+class CsvProjectRepository : ProjectRepository {
+
+    override fun getAll(): List<Project> {
+        return parseProjectData().map { raw ->
+            Project(
+                id = raw.id,
+                title = raw.title,
+                teamId = raw.teamId
+            )
+        }
+    }
+}
