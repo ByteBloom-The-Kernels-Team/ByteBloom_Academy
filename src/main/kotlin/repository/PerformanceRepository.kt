c038bea4beaada7723fbcb617571ea73749ff47a
Doaa Allaham <yahyadoaa25@gmail.com>
2025-12-25 17:18:52 +0200

    Implement-CSV-repositories-for-each-interface

--- src/main/kotlin/repository/PerformanceRepository.kt
+++ src/main/kotlin/repository/PerformanceRepository.kt
@@ -1,9 +1,8 @@
 package repository
-
 import domain.PerformanceSubmission
 
 interface PerformanceRepository {
     fun getAll(): List<PerformanceSubmission>
-    fun getById(id: String): List<PerformanceSubmission>
+    fun getByMenteeId(id: String): List<PerformanceSubmission>
     fun getByType(type: String): List<PerformanceSubmission>
 }
