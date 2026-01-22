8ee305743299ca5c78058c1a001c0f2dfb5dbfef
lamabarhoom <lamabarhoom669@gmail.com>
2026-01-22 15:27:23 +0200

    refactor: AssignMentorToMenteeUseCase using MentorSearchStrategy

--- src/main/kotlin/domain/usecase/AssignMentorToMenteeUseCase.kt
+++ src/main/kotlin/domain/usecase/AssignMentorToMenteeUseCase.kt
@@ -1,4 +1,10 @@
 package domain.usecase
 
-class AssignMentorToMenteeUseCase {
+import domain.strategy.team.MentorSearchStrategy
+
+class AssignMentorToMenteeUseCase(
+    private val mentorSearchStrategy: MentorSearchStrategy
+) {
+    fun execute(menteeId: String): String? =
+        mentorSearchStrategy.findMentorForMentee(menteeId)
 }
