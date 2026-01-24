cfed1e5e0a86b5ae69582ccdb89550424b30f6f1
lamabarhoom <lamabarhoom669@gmail.com>
2026-01-24 11:33:54 +0200

    refactor: replace strategy and service with use case for get all mentees use case

--- src/main/kotlin/domain/usecase/GetAllMenteesUseCase.kt
+++ src/main/kotlin/domain/usecase/GetAllMenteesUseCase.kt
@@ -5,6 +5,7 @@ import domain.repository.MenteeRepository
 class GetAllMenteesUseCase(
     private val menteeRepository: MenteeRepository
 ) {
-    fun execute(): List<String> =
-        menteeRepository.getAllMentees().map { it.name }
+    operator fun invoke(): List<String> =
+        menteeRepository.getAllMentees()
+            .map { it.name }
 }
