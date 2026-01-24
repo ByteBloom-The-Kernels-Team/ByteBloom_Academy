ebd9f813b52829f51f71f91008e16cde79a38b58
lamabarhoom <lamabarhoom669@gmail.com>
2026-01-24 14:29:41 +0200

    refactor: replace strategy and service with use case for get performance breakdown for mentee usecase.

--- src/main/kotlin/domain/usecase/GetPerformanceBreakdownForMenteeUseCase.kt
+++ src/main/kotlin/domain/usecase/GetPerformanceBreakdownForMenteeUseCase.kt
@@ -1,4 +1,32 @@
 package domain.usecase
 
-class GetPerformanceBreakdownForMenteeUseCase {
+import domain.model.PerformanceSubmission
+import domain.model.SubmissionType
+import domain.repository.PerformanceSubmissionRepository
+
+class GetPerformanceBreakdownForMenteeUseCase(
+    private val performanceSubmissionRepository: PerformanceSubmissionRepository
+) {
+    operator fun invoke(menteeId: String): Map<SubmissionType, List<Double>> {
+        val allPerformances = performanceSubmissionRepository.getAllPerformanceSubmissions()
+        val filteredPerformances = filterByMentee(allPerformances, menteeId)
+
+        return mapScoresByType(filteredPerformances)
+    }
+
+    private fun mapScoresByType(
+        submissions: List<PerformanceSubmission>
+    ): Map<SubmissionType, List<Double>> {
+        return submissions.groupBy { it.type }
+            .mapValues { entry ->
+                entry.value.mapNotNull { it.score.toDoubleOrNull() }
+            }
+    }
+
+    private fun filterByMentee(
+        submissions: List<PerformanceSubmission>,
+        menteeId: String
+    ): List<PerformanceSubmission> =
+         submissions.filter { it.menteeId == menteeId }
+
 }
