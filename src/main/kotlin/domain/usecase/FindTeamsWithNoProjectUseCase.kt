acee29c54a8ed2d118addbd1432ecfc008495362
lamabarhoom <lamabarhoom669@gmail.com>
2026-01-24 11:24:01 +0200

    refactor: replace strategy and service with use case for finding teams without projects

---
+++ src/main/kotlin/domain/usecase/FindTeamsWithNoProjectUseCase.kt
@@ -0,0 +1,25 @@
+package domain.usecase
+
+import domain.model.Team
+import domain.repository.TeamRepository
+import domain.repository.ProjectRepository
+
+class FindTeamsWithNoProjectUseCase(
+    private val teamRepository: TeamRepository,
+    private val projectRepository: ProjectRepository
+) {
+    operator fun invoke(): List<Team> {
+        val allTeams = teamRepository.getAllTeams()
+        val teamIdsWithProjects = findTeamIdsWithProjects()
+
+        return allTeams.filterNot { team ->
+            team.id in teamIdsWithProjects
+        }
+    }
+
+    private fun findTeamIdsWithProjects(): Set<String> {
+        return projectRepository.getAllProjects()
+            .map { it.teamId }
+            .toSet()
+    }
+}
