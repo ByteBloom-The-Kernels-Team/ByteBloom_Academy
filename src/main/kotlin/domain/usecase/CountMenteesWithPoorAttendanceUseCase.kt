179ce3a233a3d49611a1df39612a123f9b6f6332
Alaa_Mousa <https:github.com/alaamonir02>
2026-01-24 12:13:45 +0200

    add CountMenteesWithPoorAttendanceUseCase

---
+++ src/main/kotlin/domain/usecase/CountMenteesWithPoorAttendanceUseCase.kt
@@ -0,0 +1,20 @@
+package domain.usecase
+
+import domain.model.AttendanceStatus
+import domain.model.Attendance
+import domain.repository.AttendanceRepository
+
+class CountMenteesWithPoorAttendanceUseCase(
+    private val attendanceRepository: AttendanceRepository
+){
+    operator fun invoke(): Int {
+        val allAttendance = attendanceRepository.getAllAttendances()
+        return allAttendance.countPoorStatus()
+    }
+
+    private fun List<Attendance>.countPoorStatus(): Int {
+        return this.count { attendance ->
+            attendance.weeklyStatus.any { it != AttendanceStatus.PRESENT }
+        }
+    }
+}
--- src/main/kotlin/domain/usecase/SearchTeamByNameUseCase.kt
+++ src/main/kotlin/domain/usecase/SearchTeamByNameUseCase.kt
@@ -6,11 +6,11 @@ import domain.repository.TeamRepository
 class SearchTeamByNameUseCase(
     private val teamRepository: TeamRepository
 ) {
-    operator fun invoke(name: String): Team? {
+    operator fun invoke(teamName: String): Team? {
         return teamRepository.getAllTeams().findByName(teamName)
     }
-}
 
-private fun List<Team>.findByName(name: String): Team? {
-    return this.find { it.name.equals(name, ignoreCase = true) }
+    private fun List<Team>.findByName(name: String): Team? {
+        return this.find { it.name.equals(name, ignoreCase = true) }
+    }
 }
