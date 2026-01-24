27a2b1a62dd8be3647e86e3a9951f3ac0e6c61b8
lamabarhoom <lamabarhoom669@gmail.com>
2026-01-24 11:40:55 +0200

    refactor: replace strategy and service with use case for get all mentors use case

--- src/main/kotlin/domain/usecase/GetAllMentorsUseCase.kt
+++ src/main/kotlin/domain/usecase/GetAllMentorsUseCase.kt
@@ -5,6 +5,7 @@ import domain.repository.TeamRepository
 class GetAllMentorsUseCase(
     private val teamRepository: TeamRepository
 ) {
-    fun execute(): List<String> =
-        teamRepository.getAllTeams().map { it.mentor }
+    operator fun invoke(): List<String> =
+        teamRepository.getAllTeams()
+            .map { it.mentor }
 }
